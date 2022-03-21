54c4d4c1774423c6a6e9eba4e3ccd168f4420a24
z9fr <z9fr@protonmail.com>
2022-03-21 15:54:01 +0530

    Implemented the Transport pakage

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main(){
-  fmt.Println("go rest ")
+// App - the struct which contains things like
+// pointers to database connections
+type App struct{}
+
+// Run - handles the startup of our application
+func (app *App) Run() error {
+	fmt.Print("Server is Starting")
+	return nil
+}
+
+// Our main entrypoint for the application
+func main() {
+
+	app := App{}
+
+	if err := app.Run(); err != nil {
+		fmt.Print("Error running the REST api")
+		fmt.Print(err)
+	}
+
 }
---
+++ internal/transport/handler.go
@@ -0,0 +1,30 @@
+package http
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Handler - store pointer to our comment service
+type Handler struct {
+	Router *mux.Router
+}
+
+// NewHandler - return a pointer to a handler
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+// SetupRoutes - sets up all the routes for our application
+func (h *Handler) SetupRotues() {
+	fmt.Printf("Setting up routes")
+
+	// initicate new gorilla mox router
+	h.Router = mux.NewRouter()
+	h.Router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "API running ok ")
+	})
+
+}
