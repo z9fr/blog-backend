c31e7ef2f0c2d678bc974fdda81e9fffbba52aa6
z9fr <z9fr@protonmail.com>
2022-04-03 16:36:26 +0530

    github.com/z9fr/blog-backend

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -1,9 +1,10 @@
 package http
 
 import (
-	"api/internal/post"
 	"encoding/json"
 	"net/http"
+
+	"github.com/z9fr/blog-backend/internal/post"
 
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
